0a7d8ebbb7412a5c0ec2b102da4d15243da9aa4c
Laparca8 <eghinner@gmail.com>
2021-02-27 01:41:24 -0400

    Heroku-Provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Schema;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if (env('REDIRECT_HTTPS')) {
+               $this->app['request']->server->set('HTTPS', true);
+           }
     }
 
     /**
@@ -26,5 +29,9 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Schema::defaultStringLength(191);
+
+        if (env('REDIRECT_HTTPS')) {
+               $url->formatScheme('https://');
+           }
     }
 }
